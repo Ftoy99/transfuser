be83a1f0697e2accfb4756a2f32d131d1f42cad1
Ftoy <spit10000@yahoo.gr>
2025-06-01 13:36:30 +0300

    Save image

--- team_code_transfuser/submission_agent.py
+++ team_code_transfuser/submission_agent.py
@@ -298,8 +298,7 @@ class HybridAgent(autonomous_agent.AutonomousAgent):
         # prepare image input
         image = self.prepare_image(tick_data)
         img_path = os.path.join(log_dir, f"image_tick_data_proccesed.png")
-        img = Image.fromarray(image.astype(np.uint8))
-        img.save(img_path)
+        save_image(image,img_path)
 
         num_points = None
         if(self.backbone == 'latentTF'): # Image only method
