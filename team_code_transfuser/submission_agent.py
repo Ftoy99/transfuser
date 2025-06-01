56e7e622b91736fa768903af1bf4247af610e62c
Ftoy <spit10000@yahoo.gr>
2025-06-01 13:58:13 +0300

    Debug val

--- team_code_transfuser/submission_agent.py
+++ team_code_transfuser/submission_agent.py
@@ -273,6 +273,7 @@ class HybridAgent(autonomous_agent.AutonomousAgent):
         # Need to run this every step for GPS denoising
         tick_data = self.tick(input_data)
         for key, val in tick_data.items():
+            print(val)
             if key == "rgb":
                 for i, img_array in enumerate(val):
                     img_path = os.path.join(log_dir, f"rgb_tick_data_{i}.png")
